75d364ed3a8be52d2aa5c82645bc24e6eecbf656
User <User@DESKTOP-OUQDR8H>
2021-07-23 14:38:20 +0600

    Добавьте файлы проекта.

---
+++ Pages/Logs.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace BlazorEx.Pages
+{
+    public class GetText : ComponentBase
+    {
+        public int i = 0;
+        public int index = 0;
+        public int indexView = 1;
+        public string str { get; set; }
+        static string path = @".\wwwroot\Канал_03.CE 303";
+        public string[] text = File.ReadAllLines(path, CodePagesEncodingProvider.Instance.GetEncoding(1251));
+    }
+}
